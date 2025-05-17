61321a0a021859831f321eaf6dbfc018a077a814
unknown <saparbek.kozhanazar04@gmail.com>
2025-05-17 13:39:22 +0600

    Small changes

--- src/main/java/com/saparbek/task_manager_pro/auth/config/DotenvInitializer.java
+++ src/main/java/com/saparbek/task_manager_pro/auth/config/DotenvInitializer.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Component;
 
 import java.util.*;
 
-@Component
-public class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-    @Override
-    public void initialize(ConfigurableApplicationContext context) {
-        Dotenv dotenv = Dotenv.configure().load();
-        Map<String, Object> envMap = new HashMap<>();
-        dotenv.entries().forEach(entry -> envMap.put(entry.getKey(), entry.getValue()));
-        context.getEnvironment().getPropertySources().addFirst(new MapPropertySource("dotenv", envMap));
-    }
-}
+//@Component
+//public class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+//
+//    @Override
+//    public void initialize(ConfigurableApplicationContext context) {
+//        Dotenv dotenv = Dotenv.configure().load();
+//        Map<String, Object> envMap = new HashMap<>();
+//        dotenv.entries().forEach(entry -> envMap.put(entry.getKey(), entry.getValue()));
+//        context.getEnvironment().getPropertySources().addFirst(new MapPropertySource("dotenv", envMap));
+//    }
+//}
